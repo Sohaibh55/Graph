a2b4397d6c8930206740ea4745355925e0322dbb
sohaibh55 <sohaibhamdi55@outlook.com>
2025-01-17 18:57:03 -0400

    I added a new function : BFS (Breadth First Search)

--- graph.py
+++ graph.py
@@ -1,5 +1,5 @@
 
-
+import queue
 class graph :
     def __init__(self,size):
         self.graph = [ [ 0 for _ in range(size)] for _ in range(size) ]
@@ -15,4 +15,19 @@ class graph :
                if  self.graph[i][j] != 0 :
                    print(f" ( {j},{self.graph[i][j]} ) ",end='')
            print()
+    def BFS(self,start_index):
+        vertex_index = [True if _ == start_index  else False for _ in range(len(self.graph)) ]
+        Queue = queue.Queue()
+        Queue.put(start_index)
+        print(f"BFS traversal starting from node {start_index} : ",end="")
+        while not Queue.empty() :
+
+            start_index = Queue.get()
+            print(start_index,end=" ")
+
+            for i in range(len(self.graph)) :
+                if not vertex_index[i] and self.graph[start_index][i]:
+                    Queue.put(i)
+                    vertex_index[i] = True
+
 
--- main.py
+++ main.py
@@ -2,13 +2,17 @@ import graph
 
 Graph = graph.graph(5)
 
-Graph.insert(10, 1, 3, True)
+Graph.insert(0, 1, 3, True)
 Graph.insert(0, 2, 5, True)
 
-Graph.insert(1, 2, 6, True)
-graph.insert(1, 3, 8, True)
+
+Graph.insert(1, 3, 8, True)
 
 Graph.insert(2, 3, 9, True)
-Graph.insert(2, 4, 10, True)
 
-graph.display()
+Graph.insert(4, 1, 10, True)
+Graph.insert(4, 3, 15, True)
+
+Graph.display()
+
+Graph.BFS(3)
