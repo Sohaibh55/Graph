283bb9153278b31503c682021ba4185e9d7962a5
sohaibh55 <sohaibhamdi55@outlook.com>
2025-01-17 00:31:35 -0400

    we added two functions :  insert and display

--- graph.py
+++ graph.py
@@ -1,7 +1,18 @@
 
 
 class graph :
-    def __init__(self):
-        self.graph = []
-    def insert(self,value):
-        pass
+    def __init__(self,size):
+        self.graph = [ [ 0 for _ in range(size)] for _ in range(size) ]
+    def insert(self,u,v,distance,allow):
+        assert 0 <= u < len(self.graph) and  0 <= v <len(self.graph), " Error : Invalid index "
+        self.graph[u][v] = distance
+        if allow :
+            self.graph[v][u] = distance
+    def display(self):
+        for i in  range(len(self.graph)) :
+           print(f"{i} : ",end="")
+           for j in  range(len(self.graph)) :
+               if  self.graph[i][j] != 0 :
+                   print(f" ( {j},{self.graph[i][j]} ) ",end='')
+           print()
+
